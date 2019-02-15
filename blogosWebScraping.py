cc34ea3495e34a03eec49cf3df5a56b144bfe3ce
tatsuki-ichijo <46990384+tatsuki-ichijo@users.noreply.github.com>
2019-02-15 15:34:20 -0800

    Add files via upload

--- blogosWebScraping.py
+++ blogosWebScraping.py
@@ -5,13 +5,30 @@ from bs4 import BeautifulSoup
 blogos = requests.get("https://blogos.com/")
 
 # set BueatifulSoup
-soup2 = BeautifulSoup(blogos.text, "html.parser")
+soup = BeautifulSoup(blogos.text, "html.parser")
 
 # get headlines
-blogos_headlines = soup2.find("ul", attrs={"class", "side-dot"})
+blogos_headlines = soup.find_all(class_ = "pickup-article-main-column-simple-0 pickup-box")
 
-bH = blogos_headlines.get_text()
+headlines = []
+for headline in blogos_headlines:
+    for side_dot in headline.find_all(class_ = 'side-dot'):
+        for li in side_dot.find_all('li'):
+            headlines.append(li.text)
 
-from autoEmail import Mailsend
+# get links
+blogos_headlines2 = soup.find_all("div")
 
-Mailsend()
+links = []
+for link in blogos_headlines2:
+    for pickup in link.find_all(class_ = "pickup-article-main-column-simple-0"):
+        for aa in pickup.find_all('a'):
+            links.append("http://www.blogos.com"+aa.get('href'))
+
+newlinks = links[0:10]
+
+# Results
+for c , d in zip(headlines, newlinks):
+    print(c + ", " + d)
+
+
